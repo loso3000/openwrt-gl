62a4382355bfe356f5488559a57bf3135bf29003
loso3000 <51484662+loso3000@users.noreply.github.com>
2021-02-01 20:35:03 +0800

    set

--- scripts/customize-feeds-gl.sh
+++ scripts/customize-feeds-gl.sh
@@ -57,7 +57,7 @@ mkdir package/community
 cd /workdir/openwrt/package/community
 
 # Add Lienol's Packages
-git clone --depth=1 https://github.com/Lienol/openwrt-package
+#git clone --depth=1 https://github.com/Lienol/openwrt-package
 
 # Add luci-app-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
